a14c15cd9d10eae00288365c435a2faa5576c514
Onur Yazıcı <onurr.yazici@outlook.com>
2020-10-11 15:14:30 +0300

    Navigation drawer icons updated

--- lib/Widgets/NavigationDrawer.dart
+++ lib/Widgets/NavigationDrawer.dart
@@ -1,23 +1,25 @@
 import 'package:datagram/Constanst.dart';
-import 'package:datagram/Fragments/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    HomeScreen homeScreen;
-
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            child: Text('Drawer Header'),
+            child: Image.asset('assets/logo.png'),
             decoration: BoxDecoration(color: CustomColors.black),
           ),
           ListTile(
             title: Text('Drive\'ım'),
+            leading: SvgPicture.asset(
+              'assets/icons/datagram_singular_black.svg',
+              height: 25,
+              color: CustomColors.defaultGray,
+            ),
             onTap: () {
               //homeScreen = HomeScreen(this.callback)
             },
@@ -42,6 +44,11 @@ class NavigationDrawer extends StatelessWidget {
           ),
           ListTile(
             title: Text('Çöp Kutusu'),
+            leading: SvgPicture.asset(
+              'assets/icons/trash.svg',
+              height: 25,
+              color: CustomColors.defaultGray,
+            ),
             onTap: () {},
           )
         ],
